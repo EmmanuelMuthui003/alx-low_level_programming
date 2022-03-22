973147258ab546622a0a6651e99c79714c2d2557
EmmanuelMuthui003 <emmanuelmuthui14@gmail.com>
2022-03-22 16:15:12 -0700

    Print every character of a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,22 @@
 
 void rev_string(char *s)
 {
-	char *t = s;
+	int i, max, half;
+	char first, last;
 
+	i = 0;
+	while (s[i] != '\0')
 	{
-		while (*t)
-			t++;
-		while (s < t--)
-			_putchar(*t);
-		_putchar('\n');
+			i++;
+	}
+	max  i - 1;
+	half = max = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
 	}
 }
