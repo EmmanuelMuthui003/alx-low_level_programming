986f920f12c78bef7fbd2631d92b21ce84a41831
EmmanuelMuthui003 <emmanuelmuthui14@gmail.com>
2022-03-25 13:35:10 -0700

    function that encodes a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *cap_string - function that capitalizes all words of a string
+ * @str: string to be modified
+ *
+ * Return: caps on first letter of a separator
+ */
+
+char *cap_string(char *)
+{
+
