7f969182ec7d698dad82dae6625fea78f89814ca
EmmanuelMuthui003 <emmanuelmuthui14@gmail.com>
2022-04-07 10:58:56 -0700

    copy char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,39 @@
-#include "holberton.h"
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * _calloc - allocates memory for an array
- * @nmemb: Number of the array elements
- * @size: the size of elements on the Array
- * Return: void pointer to the allocated memory
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
  */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char *arr;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc(nmemb * size);
-	if (arr == NULL)
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		arr[i] = 0;
-	return (arr);
+	_memset(p, 0, (nmemb * size));
+	return (p);
 }
